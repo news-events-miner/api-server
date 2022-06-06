434df7421c583368cec0834f944c4f47070075b8
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-06-06 12:13:40 +0500

    Move routes to separate file

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,10 @@
+mod routes;
 mod types;
 
-use actix_web::{get, post, web, App, HttpServer, Responder};
-use awc;
-use std::collections::HashMap;
+use actix_web::{App, HttpServer};
+use routes::*;
 use std::env;
 use types::*;
-
-// Check Spark job status
-#[get("/job")]
-async fn get_job(_job: web::Json<Job>) -> impl Responder {
-    let existent_job = NewJob {
-        name: String::from("SomeJob"),
-        param1: String::from("value"),
-    };
-    web::Json(existent_job)
-}
-
-// Add new Spark job
-#[post("/job")]
-async fn add_job(_job: web::Json<NewJob>) -> impl Responder {
-    let new_job = Job {
-        id: String::from("someCr4zyStuff"),
-    };
-    web::Json(new_job)
-}
-
-/// Get events by within given period
-#[post("/events")]
-async fn get_events(window: web::Json<TimeWindow>) -> impl Responder {
-    let window_clone = window.clone();
-    let hashmap = HashMap::from([
-        (String::from("$gte"), window_clone.left),
-        (String::from("$lte"), window_clone.right),
-    ]);
-
-    let selector = Selector {
-        selector: DateFilter { date: hashmap },
-    };
-
-    // TODO: remove unwraps
-    let db_host = env::var("DB_HOST").unwrap();
-    let db_port = env::var("DB_PORT").unwrap();
-    let db_user = env::var("DB_USER").unwrap();
-    let db_pass = env::var("DB_PASS").unwrap();
-    let db_name = env::var("DB_NAME").unwrap();
-
-    let url = String::from(format!("http://{}:{}/{}/_find", db_host, db_port, db_name));
-
-    let client = awc::Client::default();
-    let req = client
-        .post(url)
-        .basic_auth(db_user, db_pass)
-        .content_type("application/json");
-    let mut res = req.send_json(&selector).await.unwrap();
-
-    let events = res.json::<Events>().limit(65535 * 128).await.unwrap();
-
-    web::Json(events)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
