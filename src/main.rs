5d3f9ce261280aa5d40c16cfded43edf3448140d
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2022-06-07 15:26:02 +0500

    Enable CORS

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,25 @@ mod k8s;
 mod routes;
 mod types;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use routes::*;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    println!("Starting web server");
+
+    let server = HttpServer::new(|| {
+        let cors = Cors::default().allow_any_origin().allow_any_header();
         App::new()
+            .wrap(cors)
             .service(get_job)
             .service(add_job)
             .service(get_events)
     })
     .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+    .run();
+
+    println!("Server has started successfully");
+    server.await
 }
